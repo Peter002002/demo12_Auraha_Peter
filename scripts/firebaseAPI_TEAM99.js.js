59f36c90903c9650021f70c6443a74dc8277afde
Peter002002 <peterauraha@gmail.com>
2022-11-22 23:36:00 -0800

    first commit

--- scripts/firebaseAPI_TEAM99.js.js
+++ scripts/firebaseAPI_TEAM99.js.js
@@ -2,12 +2,14 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyCHQpATTQYQ7utrrqy7RGgb9XoZkf3NcuU",
+    authDomain: "comp1800-202223demos.firebaseapp.com",
+    projectId: "comp1800-202223demos",
+    storageBucket: "comp1800-202223demos.appspot.com",
+    messagingSenderId: "949350223234",
+    appId: "1:949350223234:web:3fe191322eba9b595ad39b",
+    measurementId: "G-FWEW2JZVT0"
+
 };
 
 //--------------------------------------------
